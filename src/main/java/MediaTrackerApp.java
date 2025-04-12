51f9a924203f95cf2185b665091988c0f36d97fe
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2025-04-12 15:04:40 +0200

    feat: Initialize database, API, and HTTP client settings

--- src/main/java/MediaTrackerApp.java
+++ src/main/java/MediaTrackerApp.java
@@ -5,4 +5,10 @@ import okhttp3.*;
 import org.json.*;
 
 public class MediaTrackerApp {
+
+    private static final String DB_URL = "jdbc:sqlite:media_tracker.db";
+    private static final String TMDB_API_KEY = "139b3b69f6805fb68bd0d5ba0ad3a0fc";
+    private static final OkHttpClient client = new OkHttpClient();
+
+
 }
